04b51b6e97f8a7dcd1a109c427db452e10f85d0f
Addarsh Chandrasekar <addarsh@Addarshs-MacBook-Pro.local>
2019-08-17 14:31:07 -0700

    Added necklace component, basic page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NecklaceComponent } from './necklace/necklace.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NecklaceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/necklace/necklace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'necklace-demo',
+  templateUrl: './necklace.component.html',
+  styleUrls: ['./necklace.component.css']
+})
+export class NecklaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
